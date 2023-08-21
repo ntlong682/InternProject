846f93f0f2774b0978f3f112f7f0063bd10e0184
ntlong682 <ntlong682@gmail.com>
2023-08-21 08:19:21 +0700

    fix user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,11 @@ import { User } from "src/models/user.model";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { AuthGuard } from "src/auth/auth.guard";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
     imports:[SequelizeModule.forFeature([User])],
-    providers:[UserService, AuthGuard],
+    providers:[UserService, AuthGuard, JwtService],
     controllers: [UserController],
 })
 export class UserModule {
